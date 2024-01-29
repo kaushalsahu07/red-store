1d44d843156b164af8b4f602901f4e54caf25c65
Kaushal sahu <kaushal.r.sahu@gmail.com>
2024-01-29 16:04:09 +0530

    almost done

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 let Login = document.querySelector(".form-login");
 let Singup = document.querySelector(".form-singup");
+let line = document.querySelector(".line");
 
-function login(){
-    Login.style.transform = "translateX(-335px)";
-    Singup.style.transform = "translateX(-335px)";
+function login() {
+  Login.style.transform = "translateX(0)";
+  Singup.style.transform = "translateX(0)";
+  line.style.transform = "translateX(0)";
 }
-function singup(){
-    Singup.style.transform = "translateX(00px)";
-    Login.style.transform = "translateX(00px)";
-
-    console.log("helo");
+function singup() {
+  Singup.style.transform = "translateX(-335px)";
+  Login.style.transform = "translateX(-335px)";
+  line.style.transform = "translateX(112px)";
 }
