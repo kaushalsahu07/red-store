0ff05cce62e99d4ded5c802faeb17b92002261bf
Kaushal sahu <kaushal.r.sahu@gmail.com>
2024-02-23 17:29:27 +0530

    nav

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ function signin() {
   Login.style.transform = "translateX(-335px)";
   line.style.transform = "translateX(110px)";
 }
+
+// nav
+let navUl = document.querySelector("#ul");
+let navMenu = document.querySelector(".nav-menu");
+let navIcon = document.querySelector("#bars");
+
+navMenu.onclick = function() {
+  if(navUl.style.left == "-70pc" || navIcon.className == "fa-solid fa-bars-staggered"){
+    navUl.style.left = "0px";
+    navIcon.className = "fa-solid fa-xmark"
+  }else{
+    navUl.style.left = "-70pc";
+    navIcon.className = "fa-solid fa-bars-staggered";
+
+  }
+}
