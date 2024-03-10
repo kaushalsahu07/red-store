cc514c1965d95c56109da0fbd1fee77206f58ca4
Kaushal sahu <kaushal.r.sahu@gmail.com>
2024-03-10 21:04:42 +0530

    making for moblie

--- main.js
+++ main.js
@@ -13,24 +13,42 @@ function signin() {
   line.style.transform = "translateX(110px)";
 }
 
+function screen() {
+  if (window.innerWidth <= 1080) {
+    function login() {
+      Login.style.transform = "translateX(0)";
+      Signin.style.transform = "translateX(0)";
+      line.style.transform = "translateX(0)";
+    }
+    function signin() {
+      Signin.style.transform = "translateX(-200px)";
+      Login.style.transform = "translateX(-200px)";
+      line.style.transform = "translateX(100px)";
+    }
+  }
+}
+
+screen();
 // nav
 let navUl = document.querySelector("#ul");
 let navMenu = document.querySelector(".nav-menu");
 let navIcon = document.querySelector("#bars");
 
-navMenu.onclick = function() {
-  if(navUl.style.left == "-70pc" || navIcon.className == "fa-solid fa-bars-staggered"){
+navMenu.onclick = function () {
+  if (
+    navUl.style.left == "-70pc" ||
+    navIcon.className == "fa-solid fa-bars-staggered"
+  ) {
     navUl.style.left = "0px";
-    navIcon.className = "fa-solid fa-xmark"
-  }else{
+    navIcon.className = "fa-solid fa-xmark";
+  } else {
     navUl.style.left = "-70pc";
     navIcon.className = "fa-solid fa-bars-staggered";
-
   }
-}
+};
 //img
 let proImg = document.querySelector("#product-img");
 
-function galleryImg(img){
-  proImg.src = img
+function galleryImg(img) {
+  proImg.src = img;
 }
